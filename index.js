e14b4a2fd913edd75043bd331a465f26f82a3b74
Artem1998 <arty-hero@yandex.ru>
2018-05-09 13:22:59 -0400

    Add root route

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res)+>{
+  res.send({bye: 'buddy'});
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
