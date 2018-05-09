1aaef3627d2cb687aeb6a09a07be99123fd68c98
Artem1998 <arty-hero@yandex.ru>
2018-05-09 13:30:28 -0400

    Change index file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)+>{
+app.get('/', (req, res)=>{
   res.send({bye: 'buddy'});
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(5000, ()=>{
+  console.log('Started on port 5000');
+});
