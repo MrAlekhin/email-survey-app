19432d2fbcd79f38cc7b16dbf3462e36ae4121a5
Artem1998 <arty-hero@yandex.ru>
2018-05-10 16:45:07 -0400

    Add OAuthentication

---
+++ services/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    done(null, await User.findById(id));
+  } catch (e) {}
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleID: profile.id })
+          .save()
+          .then(user => done(null, user));
+      }
+    }
+  )
+);
